d7587e3921dfff387d81c3c837535e73b477d897
Daryl Legion <whoamisnowden@gmail.com>
2019-04-24 01:34:02 +0800

    changes slider images

--- resources/views/layouts/frontend/inc/carousel.blade.php
+++ resources/views/layouts/frontend/inc/carousel.blade.php
@@ -4,7 +4,7 @@
         	<img src="{{ URL::asset('assets/frontend/images/slider/slider-large1.jpg') }}" alt="Slider Image">
             <div class="blog-post">
                 <div class="blog-outer">
-                	<h3 class="blog-title"><a href="#">Who i am - A Mystery Begins</a></h3>
+                	<h3 class="blog-title"><a href="#">Education is the key to unlock the golden door of freedom.</a></h3>
                 </div>
             </div>
         </div>
@@ -12,7 +12,7 @@
         	<img src="{{ URL::asset('assets/frontend/images/slider/slider-large2.jpg') }}" alt="Slider Image">
             <div class="blog-post">
                 <div class="blog-outer">
-                    <h3 class="blog-title"><a href="#">Local Tram In A City</a></h3>
+                    <h3 class="blog-title"><a href="#">Education is the key to unlock the golden door of freedom.</a></h3>
                 </div>
             </div>
         </div>
@@ -20,7 +20,7 @@
         	<img src="{{ URL::asset('assets/frontend/images/slider/slider-large3.jpg') }}" alt="Slider Image">
             <div class="blog-post">
                 <div class="blog-outer">
-                   	<h3 class="blog-title"><a href="#">Bahama's Trip: Waiting For Our Flight</a></h3>                    
+                   	<h3 class="blog-title"><a href="#">Education is the key to unlock the golden door of freedom.</a></h3>                    
                 </div>
             </div>
         </div>
@@ -28,7 +28,7 @@
         	<img src="{{ URL::asset('assets/frontend/images/slider/slider-large4.jpg') }}" alt="Slider Image">
             <div class="blog-post">
                 <div class="blog-outer">
-                   	<h3 class="blog-title"><a href="#">Journey By A Vintage Car</a></h3>                     
+                   	<h3 class="blog-title"><a href="#">Future Of Education Technology</a></h3>                     
                 </div>
             </div>
         </div>
@@ -36,7 +36,7 @@
         	<img src="{{ URL::asset('assets/frontend/images/slider/slider-large5.jpg') }}" alt="Slider Image">
             <div class="blog-post">
                 <div class="blog-outer">
-                   	<h3 class="blog-title"><a href="#">Skyview Shot From My Home</a></h3>                     
+                   	<h3 class="blog-title"><a href="#">Education is the key to unlock the golden door of freedom.</a></h3>                     
                 </div>
             </div>
         </div> 
