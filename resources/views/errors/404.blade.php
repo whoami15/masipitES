f02e9e951459f82e0b7e19b4026a9afa61b3bc60
Daryl Legion <whoamisnowden@gmail.com>
2019-04-25 17:05:23 +0800

    adds custom error handling pages

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>404 Not Found</title>
+		<meta name="google-site-verification" content="qW1wk8CNryMR2Ilv6nwYL9NTmrppzUCq0NFkDaCSrgY" />
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">		
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                color: #B0BEC5;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 72px;
+                margin-bottom: 20px;
+            }
+            .subtitle {
+                font-size: 32px;
+                color: rgb(0,122,255);
+                margin-bottom: 40px;
+                font-weight: 600;
+            }
+			.m-b-10{
+				margin-bottom:10px;
+			}
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">404 Not Found.</div>
+                <div class="m-b-10">Contact us</div>
+                <div class="subtitle"> masipitES@gmail.com / +(043)000-0000</div>
+            </div>
+        </div>	
+    </body>
+</html>
